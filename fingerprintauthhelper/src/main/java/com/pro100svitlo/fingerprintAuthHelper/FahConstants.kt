481a402c9f050b615f1a2d3ea07fac78b94ca82a
Adrian <Foxytine31@users.noreply.github.com>
2017-02-05 18:38:31 -0500

    Revert "Develop" (#3)

--- fingerprintauthhelper/src/main/java/com/pro100svitlo/fingerprintAuthHelper/FahConstants.kt
+++ fingerprintauthhelper/src/main/java/com/pro100svitlo/fingerprintAuthHelper/FahConstants.kt
@@ -5,18 +5,13 @@ package com.pro100svitlo.fingerprintAuthHelper
  */
 object FahConstants{
 
-    @JvmStatic
     val TAG = FingerprintAuthHelper::class.java.simpleName!!
-    @JvmStatic
     val DEF_TRY_TIME_OUT = (45 * 1000).toLong()
     object TimeOutService{
-        @JvmStatic
         val KEY_TRY_TIME_OUT = "KEY_TRY_TIME_OUT"
-        @JvmStatic
         val TIME_OUT_BROADCAST = "TIME_OUT_BROADCAST"
     }
     object Manager{
-        @JvmStatic
         val KEY_TIME_OUT_LEFT = "KEY_TIME_OUT_LEFT"
     }
 }
