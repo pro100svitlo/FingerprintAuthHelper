e9ce44f628ca0530af3371de1a2d2aa50b230a8f
Vitaliy Popov <ultraon@mail.ru>
2017-03-03 15:55:56 +0200

    Updated gradle version, Android SDK, .gitignore, kotlin version

---
+++ fingerprintauthhelper/src/main/kotlin/com/pro100svitlo/fingerprintAuthHelper/FahListener.kt
@@ -0,0 +1,9 @@
+package com.pro100svitlo.fingerprintAuthHelper
+
+/**
+ * Created by pro100svitlo on 8/26/16.
+ */
+interface FahListener {
+    fun onFingerprintStatus(authSuccessful: Boolean, errorType: Int, errorMess: CharSequence?)
+    fun onFingerprintListening(listening: Boolean, milliseconds: Long)
+}
