d52e111099f55b848e827693b59965a9043b1f8c
Bombo <Aku.tur@gmx.de>
2017-04-12 15:28:00 +0200

    Some refactoring of FingerprintAuthHelper (with backwards compatibility for Java and Kotlin) - Removed hungarian notation. - Changed most private properties with extra getter functions into public ones with custom getters. - Changed isSdkVersionOk from function to lazily initialized property. - Changed FahManager reference to be immutable and initialize it immediately with either a correct reference or null. This removed the need for the !! operator or even ?. with proper null checks and implicit casts. - Added JvmName annotations to properties, where the implicit getter name didn't match the removed getter function. - Deprecated explicit getter functions, which should not be used anymore and set bogus JvmName annotations to avoid naming clashes with Java names.

--- fingerprintauthhelper/src/main/kotlin/com/pro100svitlo/fingerprintAuthHelper/FingerprintAuthHelper.kt
+++ fingerprintauthhelper/src/main/kotlin/com/pro100svitlo/fingerprintAuthHelper/FingerprintAuthHelper.kt
@@ -1,265 +1,295 @@
 package com.pro100svitlo.fingerprintAuthHelper
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.support.annotation.NonNull
 import android.util.Log
 
 /**
  * Created by pro100svitlo on 11/23/16.
  */
+@Suppress("unused")
+@SuppressLint("NewApi")
 class FingerprintAuthHelper private constructor(b: Builder) {
 
-    private var mFahManager: FahManager? = null
-    private var mFahSecurityDialog: FahSecureSettingsDialog? = null
-    private var mContext: Context? = null
+    private var fahSecurityDialog: FahSecureSettingsDialog? = null
+    private var context: Context = b.context
+    private var loggingEnable = b.loggingEnable
+    private var canListenBySystem = false
 
-    private var mTimeOutLeft: Long = 0
-    private var mTriesCountLeft: Int = 0
-    private var mLoggingEnable = false
-    private var mCanListenByUser = true
-    private var mCanListenBySystem = false
-    private var mIsHardwareEnable = false
-    private var mIsListening = false
-    private var mIsFingerprintEnrolled = false
-    private var mIsTimeOutCleaned = false
-
-    init {
-        mContext = b.mContext
-        mLoggingEnable = b.mLoggingEnable
-
-        if (isSdkVersionOk()) {
-            mFahManager = FahManager(b.mContext!!, b.mListener, b.mKeyName, mLoggingEnable, b.mTimeOut)
-            mTimeOutLeft = mFahManager!!.mTimeOutLeft
+    val isSdkVersionOk: Boolean by lazy {
+        logThis("isSdkVersionOk called")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            logThis("sdkVersionOk")
+            return@lazy true
         }
+        logThis("fingerprintAuthHelper cant work with sdk version < 23 (Android M)")
+        return@lazy false
     }
+
+    @JvmName("_isSdkVersionOk")
+    @Deprecated("Use isSdkVersionOk property instead", ReplaceWith("isSdkVersionOk"))
+    fun isSdkVersionOk() = isSdkVersionOk
+
+    private val fahManager = if (isSdkVersionOk) FahManager(b.context, b.listener, b.keyName, loggingEnable, b.timeOut) else null
+
+    var timeOutLeft = fahManager?.mTimeOutLeft ?: -1
+        get() {
+            logThis("getTimeOutLeft called")
+            if (fahManager == null) {
+                serviceNotEnable("getTimeOutLeft")
+                return -1
+            }
+            field = fahManager.mTimeOutLeft
+            logThis("timeOutLeft = $field millisecond")
+            return field
+
+        }
+        private set
+
+    @JvmName("_getTimeOutLeft")
+    @Deprecated("Use getTimeOutLeft property instead", ReplaceWith("timeOutLeft"))
+    fun getTimeOutLeft() = timeOutLeft
+
+    var triesCountLeft: Int = 0
+        get() {
+            logThis("getTriesCountLeft called")
+            if (fahManager == null) {
+                serviceNotEnable("getTriesCountLeft")
+                return 0
+            }
+            field = fahManager.mTriesCountLeft
+            logThis("triesCountLeft = $field")
+            return field
+        }
+        private set
+
+    @JvmName("_getTriesCountLeft")
+    @Deprecated("Use getTriesCountLeft property instead", ReplaceWith("triesCountLeft"))
+    fun getTriesCountLeft() = triesCountLeft
+
+    var canListenByUser = true
+        @JvmName("canListenByUser")
+        get() {
+            logThis("canListenByUser called")
+            logThis("canListenByUser = $field")
+            return field
+        }
+        set(value) {
+            logThis("setCanListenByUser called")
+            logThis("setCanListenByUser = $value")
+            canListenByUser = value
+        }
+
+    @JvmName("_canListenByUser")
+    @Deprecated("Use canListenByUser property instead", ReplaceWith("canListenByUser"))
+    fun canListenByUser() = canListenByUser
+
+    @JvmName("_setCanListenByUser")
+    @Deprecated("Use canListenByUser property instead", ReplaceWith("canListenByUser = canListen"))
+    fun setCanListenByUser(canListen: Boolean) = { canListenByUser = canListen }
+
+    var isHardwareEnable = false
+        get() {
+            logThis("isHardwareEnable called")
+            if (fahManager == null) {
+                serviceNotEnable("isHardwareEnable")
+                return false
+            }
+            field = fahManager.isHardwareEnabled()
+            logThis("isHardwareEnable = $field")
+            return field
+        }
+        private set
+
+    @JvmName("_isHardwareEnable")
+    @Deprecated("Use isHardwareEnable property instead", ReplaceWith("isHardwareEnable"))
+    fun isHardwareEnable() = isHardwareEnable
+
+    var isListening = false
+        get() {
+            logThis("isListening called")
+            if (fahManager == null) {
+                serviceNotEnable("isListening")
+                return false
+            }
+            field = fahManager.isListening()
+            logThis("isListening = $field")
+            return field
+        }
+        private set
+
+    @JvmName("_isListening")
+    @Deprecated("Use isListening property instead", ReplaceWith("isListening"))
+    fun isListening() = isListening
+
+    var isFingerprintEnrolled = false
+        get() {
+            if (fahManager == null) {
+                serviceNotEnable("isFingerprintEnrolled")
+                return false
+            }
+            field = fahManager.isFingerprintEnrolled()
+            logThis("isFingerprintEnrolled = " + isFingerprintEnrolled)
+            return field
+        }
+        private set
+
+    @JvmName("_isFingerprintEnrolled")
+    @Deprecated("Use isFingerprintEnrolled property instead", ReplaceWith("isFingerprintEnrolled"))
+    fun isFingerprintEnrolled() = isFingerprintEnrolled
+
+    var isTimeOutCleaned = false
+        @JvmName("cleanTimeOut")
+        get() {
+            logThis("cleanTimeOut called")
+            if (fahManager == null) {
+                serviceNotEnable("cleanTimeOut")
+                return false
+            }
+            isTimeOutCleaned = fahManager.cleanTimeOut()
+            logThis("timeOutCleaned = $isTimeOutCleaned")
+            return isTimeOutCleaned
+        }
+        private set
+
+    @JvmName("_cleanTimeOut")
+    @Deprecated("Use isTimeOutClean property instead", ReplaceWith("isTimeOutCleaned"))
+    fun cleanTimeOut() = isTimeOutCleaned
 
     fun startListening(): Boolean {
         logThis("startListening called")
-        if (mFahManager == null) {
+        if (fahManager == null) {
             serviceNotEnable("startListening")
             return false
         }
-        if (!canListenByUser()) {
+        if (!canListenByUser) {
             return false
         }
-        mIsListening = mFahManager!!.startListening() && mTimeOutLeft <= 0
-        logThis("mIsListening = " + mIsListening)
-        return mIsListening
+        isListening = fahManager.startListening() && timeOutLeft <= 0
+        logThis("isListening = $isListening")
+        return isListening
     }
 
     fun stopListening(): Boolean {
         logThis("stopListening called")
-        if (mFahManager == null) {
+        if (fahManager == null) {
             serviceNotEnable("stopListening")
             return false
         }
-        if (!canListenByUser()) {
+        if (!canListenByUser) {
             return false
         }
-        mIsListening = mFahManager!!.stopListening()
-        logThis("mIsListening = " + mIsListening)
-        return mIsListening
+        isListening = fahManager.stopListening()
+        logThis("isListening = $isListening")
+        return isListening
     }
 
     fun onSaveInstanceState(outState: Bundle): Boolean {
         logThis("onSaveInstanceState called")
-        if (mFahManager == null) {
+        if (fahManager == null) {
             serviceNotEnable("onSaveInstanceState")
             return false
         }
-        mFahManager!!.onSaveInstanceState(outState)
+        fahManager.onSaveInstanceState(outState)
         logThis("onSaveInstanceState successful")
         return true
     }
 
     fun onRestoreInstanceState(savedInstanceState: Bundle): Boolean {
         logThis("onRestoreInstanceState called")
-        if (mFahManager == null) {
+        if (fahManager == null) {
             serviceNotEnable("onRestoreInstanceState")
             return false
         }
-        mFahManager!!.onRestoreInstanceState(savedInstanceState)
+        fahManager.onRestoreInstanceState(savedInstanceState)
         logThis("onRestoreInstanceState successful")
         return true
     }
 
     fun onDestroy(): Boolean {
         logThis("onDestroy called")
-        mContext = null
 
-        if (mFahManager == null) {
+        if (fahManager == null) {
             serviceNotEnable("onDestroy")
             return false
         }
-        mFahManager!!.onDestroy()
-        mFahManager = null
+        fahManager.onDestroy()
         logThis("onDestroy successful")
         return true
     }
 
-    fun isListening(): Boolean {
-        logThis("isListening called")
-        if (mFahManager == null) {
-            serviceNotEnable("isListening")
-            return false
-        }
-        mIsListening = mFahManager!!.isListening()
-        logThis("isListening = " + mIsListening)
-        return mIsListening
-    }
-
     fun canListen(showError: Boolean): Boolean {
         logThis("canListen called")
-        if (mFahManager == null) {
+        if (fahManager == null) {
             serviceNotEnable("canListen")
             return false
         }
-        canListenByUser()
-        mCanListenBySystem = mFahManager!!.canListen(showError)
-        logThis("canListenBySystem = " + mCanListenBySystem)
-        logThis("can listen = " + (mCanListenByUser && mCanListenBySystem).toString())
-        return mCanListenByUser && mCanListenBySystem
-    }
-
-    fun canListenByUser(): Boolean {
-        logThis("canListenByUser called")
-        logThis("canListenByUser = " + mCanListenByUser)
-        return mCanListenByUser
-    }
-
-    fun setCanListenByUser(canListen: Boolean) {
-        logThis("setCanListenByUser called")
-        logThis("setCanListenByUser = " + canListen)
-        mCanListenByUser = canListen
-    }
-
-    fun getTimeOutLeft(): Long {
-        logThis("getTimeOutLeft called")
-        if (mFahManager == null) {
-            serviceNotEnable("getTimeOutLeft")
-            return -1
-        }
-        mTimeOutLeft = mFahManager!!.mTimeOutLeft
-        logThis("timeOutLeft = $mTimeOutLeft millisecond")
-        return mTimeOutLeft
-    }
-
-    fun getTriesCountLeft(): Int {
-        logThis("getTriesCountLeft called")
-        if (mFahManager == null) {
-            serviceNotEnable("getTriesCountLeft")
-            return 0
-        }
-        mTriesCountLeft = mFahManager!!.mTriesCountLeft
-        logThis("triesCountLeft = $mTriesCountLeft")
-        return mTriesCountLeft
-    }
-
-    fun cleanTimeOut(): Boolean {
-        logThis("cleanTimeOut called")
-        if (mFahManager == null) {
-            serviceNotEnable("cleanTimeOut")
-            return false
-        }
-        mIsTimeOutCleaned = mFahManager!!.cleanTimeOut()
-        logThis("timeOutCleaned = " + mIsTimeOutCleaned)
-        return mIsTimeOutCleaned
-    }
-
-    fun isHardwareEnable(): Boolean {
-        logThis("isHardwareEnable called")
-        if (mFahManager == null) {
-            serviceNotEnable("isHardwareEnable")
-            return false
-        }
-        mIsHardwareEnable = mFahManager!!.isHardwareEnabled()
-        logThis("mIsHardwareEnable = " + mIsHardwareEnable)
-        return mIsHardwareEnable
-    }
-
-    fun isFingerprintEnrolled(): Boolean {
-        if (mFahManager == null) {
-            serviceNotEnable("isFingerprintEnrolled")
-            return false
-        }
-        mIsFingerprintEnrolled = mFahManager!!.isFingerprintEnrolled()
-        logThis("mIsFingerprintEnrolled = " + mIsFingerprintEnrolled)
-        return mIsFingerprintEnrolled
+        canListenByUser
+        canListenBySystem = fahManager.canListen(showError)
+        logThis("canListenBySystem = $canListenBySystem")
+        logThis("can listen = ${canListenByUser && canListenBySystem}")
+        return canListenByUser && canListenBySystem
     }
 
     fun openSecuritySettings() {
         logThis("openSecuritySettings called")
-        mContext?.startActivity(Intent(Settings.ACTION_SECURITY_SETTINGS))
+        context.startActivity(Intent(Settings.ACTION_SECURITY_SETTINGS))
     }
 
     fun showSecuritySettingsDialog() {
-        if (mFahSecurityDialog == null && mContext != null) {
-            mFahSecurityDialog = FahSecureSettingsDialog.Builder(mContext!!, this)
+        if (fahSecurityDialog == null) {
+            fahSecurityDialog = FahSecureSettingsDialog.Builder(context, this)
                     .build()
         }
-        mFahSecurityDialog?.show()
-    }
-
-    private fun isSdkVersionOk(): Boolean {
-        logThis("isSdkVersionOk called")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            logThis("sdkVersionOk")
-            return true
-        }
-        logThis("fingerprintAuthHelper cant work with sdk version < 23 (Android M)")
-        return false
+        fahSecurityDialog?.show()
     }
 
     private fun logThis(mess: String) {
-        if (mLoggingEnable) Log.d(FahConstants.TAG, mess)
+        if (loggingEnable) Log.d(FahConstants.TAG, mess)
     }
 
     private fun serviceNotEnable(methodName: String) {
         logThis("method '$methodName' can't be finished, because of fingerprintService not enable")
     }
 
-    class Builder(@NonNull c: Context, l: FahListener) {
+    class Builder(c: Context, internal val listener: FahListener) {
 
-        internal var mTimeOut = FahConstants.DEF_TRY_TIME_OUT
-        internal var mContext: Context? = null
-        internal val mListener: FahListener? = l
-        internal var mKeyName = FahConstants.TAG
-        internal var mLoggingEnable: Boolean = false
+        internal var timeOut = FahConstants.DEF_TRY_TIME_OUT
+        internal var context: Context
+        internal var keyName = FahConstants.TAG
+        internal var loggingEnable: Boolean = false
 
         init {
             if (c is Activity) {
-                mContext = c
+                context = c
             } else {
                 throw IllegalArgumentException("Context for FingerprintAuthHelper must be instance of Activity")
             }
-            mContext = c
         }
 
         fun setKeyName(keyName: String): Builder {
-            mKeyName = keyName
+            this.keyName = keyName
             return this
         }
 
         fun setLoggingEnable(enable: Boolean): Builder {
-            mLoggingEnable = enable
+            loggingEnable = enable
             return this
         }
 
         fun setTryTimeOut(milliseconds: Long): Builder {
             if (milliseconds < FahConstants.DEF_TRY_TIME_OUT) {
-                throw IllegalArgumentException("tryTimeout must be more than " + FahConstants.DEF_TRY_TIME_OUT + " milliseconds!")
+                throw IllegalArgumentException("tryTimeout must be more than ${FahConstants.DEF_TRY_TIME_OUT} milliseconds!")
             }
-            mTimeOut = milliseconds
+            timeOut = milliseconds
             return this
         }
 
-        fun build(): FingerprintAuthHelper {
-            return FingerprintAuthHelper(this)
-        }
+        fun build() = FingerprintAuthHelper(this)
     }
 }
